8bd8d50ec2db9abb7b39b001a39e8828e597fbfe
Vjeko <vjeko.pavicin@gmail.com>
2025-08-08 18:44:45 +0200

    Add `useGetAllPosts` hook to fetch and return the latest blog posts from Contentful

---
+++ src/hooks/useGetAllPosts.tsx
@@ -0,0 +1,26 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+export const useGetAllPosts = () => {
+  const {
+    allContentfulBlogPost: { nodes },
+  } = useStaticQuery(graphql`
+    query allBlogPostsQuery {
+      allContentfulBlogPost(
+        sort: { fields: createdAt, order: DESC }
+        limit: 3
+      ) {
+        nodes {
+          title
+          date
+          image {
+            gatsbyImageData(width: 200)
+          }
+          text {
+            raw
+          }
+        }
+      }
+    }
+  `)
+  return nodes
+}
